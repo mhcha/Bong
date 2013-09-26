a0cbf91cb9c8088a5eeb4e6aef570ee8a4074efe
mhcha <mhcha74@naver.com>
2013-09-26 10:42:32 +0900

    초기 작업 업데이트

---
+++ Bong/Bong.DB/Bong.DB/Schema Objects/Bong.DB.rolememberships.sql
@@ -0,0 +1,10 @@
+﻿EXECUTE sp_addrolemember @rolename = N'db_owner', @membername = N'Bong';
+
+
+GO
+EXECUTE sp_addrolemember @rolename = N'db_datareader', @membername = N'Bong';
+
+
+GO
+EXECUTE sp_addrolemember @rolename = N'db_datawriter', @membername = N'Bong';
+
---
+++ Bong/Bong.Web/Bong.Web.Admin/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Bong.Web.Admin.MvcApplication" Language="C#" %>
